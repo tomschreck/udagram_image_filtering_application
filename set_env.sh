2ed0ede731f924a8a282296fc68c661d22c2389a
tomschreck <tom@schreckmail.com>
2020-06-20 14:41:33 -0500

    break monlith into microservices

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,14 @@
-# This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
-export URL=http://localhost:8100
+#!/bin/bash
+
+# This file handles setting the secrets for dotenv in the CI/CD pipeline 
+# It uses the environment variables set in Travis CI to write them into the .env file
+
+echo POSTGRES_USERNAME=$POSTGRES_USERNAME >> .env
+echo POSTGRES_PASSWORD=$POSTGRES_PASSWORD >> .env
+echo POSTGRES_HOST=$POSTGRES_HOST >> .env
+echo POSTGRES_DB=$POSTGRES_DB >> .env
+echo AWS_BUCKET=$AWS_BUCKET >> .env
+echo AWS_REGION=$AWS_REGION >> .env
+echo AWS_PROFILE=$AWS_PROFILE >> .env
+echo JWT_SECRET=$JWT_SECRET >> .env
+echo URL=http://localhost:8100 >> .env
